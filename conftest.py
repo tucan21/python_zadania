f36147bedde446305d0e987e76014df7aa5107f6
tucan21 <stat21@interia.pl>
2017-04-29 18:10:12 +0200

    fixtura ogolna all tests

--- conftest.py
+++ conftest.py
@@ -1,9 +1,16 @@
 import pytest
 from fixture.aplikacja_grupy import Aplikacja_grupy
+from fixture.aplikacja_kontakty import Aplikacja_kontakty
 
 
-@pytest.fixture
+@pytest.fixture(scope = "session")
 def app(request):
     fixture = Aplikacja_grupy()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+@pytest.fixture(scope = "session")
+def app(request):
+    fixture = Aplikacja_kontakty()
     request.addfinalizer(fixture.destroy)
     return fixture
