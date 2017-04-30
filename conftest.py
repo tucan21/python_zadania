adc81472079aa373e9ed2b6b88e8e55d228358f8
tucan21 <stat21@interia.pl>
2017-04-30 12:08:22 +0200

    grupy

--- conftest.py
+++ conftest.py
@@ -1,6 +1,6 @@
 import pytest
 from fixture.aplikacja_grupy import Aplikacja_grupy
-from fixture.aplikacja_kontakty import Aplikacja_kontakty
+
 
 
 @pytest.fixture(scope = "session")
@@ -9,8 +9,3 @@ def app(request):
     request.addfinalizer(fixture.destroy)
     return fixture
 
-@pytest.fixture(scope = "session")
-def app(request):
-    fixture = Aplikacja_kontakty()
-    request.addfinalizer(fixture.destroy)
-    return fixture
